4435076efdcf554fdc98c71cc42385bd42d616ee
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-10-19 23:51:15 -0600

    Add profile generation stuff

---
+++ vk_profile_generator.py
@@ -0,0 +1,74 @@
+from glob import glob
+from votekit.ballot_generator import (
+    BlocSlateConfig,
+    slate_pl_profile_generator,
+    slate_bt_profile_generator,
+    cambridge_profile_generator,
+)
+from joblib import Parallel, delayed
+from joblib_progress import joblib_progress
+import json
+from pathlib import Path
+import random
+import numpy as np
+import os
+
+random.seed(42)
+np.random.seed(42)
+
+if os.environ.get("PYTHONHASHSEED") != "0":
+    raise ValueError(
+        "You must set the PYTHONHASHSEED environment variable to 0 for "
+        "reproducibility. If using UV, you may then rerun this script "
+    )
+
+
+generator_name_to_function = {
+    "slate_pl": slate_pl_profile_generator,
+    "slate_bt": slate_bt_profile_generator,
+    "cambridge": cambridge_profile_generator,
+}
+
+
+def process_settings_file(settings_file, profile_folder, mode):
+    with open(settings_file, "r") as f:
+        settings = json.load(f)
+
+    config = BlocSlateConfig(
+        n_voters=100_000,
+        slate_to_candidates=settings["slate_to_candidates"],
+        bloc_proportions=settings["bloc_proportions"],
+        cohesion_mapping=settings["cohesion_parameters"],
+    )
+
+    config.set_dirichlet_alphas(settings["alphas"])
+    setting_file_stem = Path(settings_file).stem
+
+    output_file = (
+        profile_folder
+        / f"{setting_file_stem.replace('sample_settings', 'profile')}.csv"
+    )
+    profile = generator_name_to_function[mode](config)
+    profile.to_csv(output_file)
+
+
+if __name__ == "__main__":
+    district_nums = [8, 10, 16, 20, 40, 80]
+    for district_num in district_nums:
+        for mode in ["slate_pl", "slate_bt", "cambridge"]:
+            settings_folder = Path(f"./vk_run_settings/{district_num}")
+            profile_folder = Path(f"./vk_voter_profiles/{mode}/{district_num}")
+            profile_folder.mkdir(exist_ok=True, parents=True)
+
+            all_settings_files = glob(f"{settings_folder}/*.json")
+
+            all_settings_files = all_settings_files
+
+            with joblib_progress(
+                description=f"Generating VK profiles for {district_num:02d} districts and voter model {mode}",
+                total=len(all_settings_files),
+            ):
+                Parallel(n_jobs=-1)(
+                    delayed(process_settings_file)(settings_file, profile_folder, mode)
+                    for settings_file in all_settings_files
+                )
