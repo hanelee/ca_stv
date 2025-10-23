515bccbea1529eb022b7ae265c8a6e3bad987bbd
Matt Jones <80478926+MattJonesMath@users.noreply.github.com>
2025-10-23 10:34:28 -0400

    Code to study racial representation

---
+++ run_all_experiments_parallel_racial.py
@@ -0,0 +1,61 @@
+from glob import glob
+from votekit.elections import FastSTV as STV, Plurality
+from votekit import RankProfile
+from joblib import Parallel, delayed
+from joblib_progress import joblib_progress
+from pathlib import Path
+import json
+import random
+import numpy as np
+import time
+
+# random.seed(42)
+# np.random.seed(42)
+
+
+def process_profile(profile_file, n_seats):
+    profile: RankProfile = RankProfile.from_csv(profile_file)
+
+    if n_seats > 1:
+        winner_list = [
+            next(iter(winner_singleton))
+            for winner_singleton in STV(
+                profile, m=n_seats, simultaneous=False
+            ).get_elected()
+        ]
+    else:
+        winner_list = [
+            next(iter(winner_singleton))
+            for winner_singleton in Plurality(profile, m=1).get_elected()
+        ]
+
+    return winner_list
+
+
+if __name__ == "__main__":
+    for model in ["slate_pl", "slate_bt", "cambridge"]:
+        output_dir = Path(f"./vk_ca_election_model_results_racial/{model}")
+        output_dir.mkdir(exist_ok=True, parents=True)
+
+        district_nums = [8, 10, 16, 20, 40, 80]
+        winners_count = [5, 4, 5, 4, 1, 1]
+        for district_num, n_seats in zip(district_nums, winners_count):
+            profile_folder = Path(f"./vk_voter_profiles_racial/{model}/")
+
+            all_profile_files = glob(f"{profile_folder}/{district_num}/*.csv")
+
+            with joblib_progress(
+                description=f"Running elections for {district_num:02d} districts and voter model {model}",
+                total=len(all_profile_files),
+            ):
+                winners_list = Parallel(n_jobs=-1)(
+                    delayed(process_profile)(profile_file, n_seats)
+                    for profile_file in all_profile_files
+                )
+
+            with open(
+                output_dir
+                / f"ca_{district_num}_districts_{n_seats}_winners_for_voter_model_{model}.json",
+                "w",
+            ) as out_file:
+                json.dump(winners_list, out_file, indent=2)
---
+++ vk_settings_generator_racial.py
@@ -0,0 +1,76 @@
+import json
+import geopandas as gpd
+from pathlib import Path
+import jsonlines as jl
+from tqdm import tqdm
+from gerrychain import Graph
+
+
+if __name__ == "__main__":
+    # Just grab the population data
+    df = gpd.read_file(
+        "./geographic_data/ca_districtr_bg_view_v1.gpkg",
+        layer="ca_districtr_bg_view_v1",
+    )
+
+    # Just a quick sanity check to make sure the graph and df line up
+    bg_graph = Graph.from_json("./geographic_data/CA_bg_2020_dualgraph.json")
+    for i, node in enumerate(bg_graph.nodes()):
+        if node != df.iloc[i]["path"]:
+            raise ValueError(f"Node {i} with id {node} does not align with df!")
+
+    df = df[["total_vap_20", "hvap_20"]]
+
+    # ========================================
+    #   IMPORTANT PARAMETERS FOR SUBSAMPLING
+    # ========================================
+    chain_length = 1000
+    n_subsamples = 5
+    subsample_interval = chain_length // n_subsamples
+
+    district_nums = [8, 10, 16, 20, 40, 80]
+    for district_num in district_nums:
+        settings_folder = Path(f"./vk_run_settings_racial/{district_num}")
+        settings_folder.mkdir(exist_ok=True, parents=True)
+
+        with jl.open(f"./districting/chain_out/ca_{district_num}_dist.jsonl") as file:
+            for sample_idx, sample in tqdm(
+                enumerate(file),
+                total=chain_length,
+                desc=f"Generating VK settings for {district_num:02d} districts",
+            ):
+
+                if sample_idx % subsample_interval != 0:
+                    continue
+
+                # Will be in the same order as the df since the graph was built from the gpkg
+                district_plan = sample["assignment"]
+
+                df["district_plan"] = district_plan
+                data_by_district = df.groupby("district_plan").sum()
+
+                for _, row in data_by_district.iterrows():
+                    district = row.name
+                    hprop = float(row["hvap_20"] / row["total_vap_20"])
+                    output_settings = dict(
+                        n_voters=100_000,
+                        slate_to_candidates={
+                            "H": ["H1", "H2", "H3"],
+                            "O": ["O1", "O2", "O3", "O4", "O5", "O6", "O7"],
+                        },
+                        bloc_proportions={"H": hprop, "O": 1 - hprop},
+                        cohesion_parameters={
+                            "H": {"H": 0.77, "O": 0.23},
+                            "O": {"H": 0.52, "O": 0.48},
+                        },
+                        alphas={
+                            "H": {"H": 0.50, "O": 0.75},
+                            "O": {"H": 0.75, "O": 0.50},
+                        },
+                    )
+
+                    with open(
+                        f"{settings_folder}/ca_{district_num}_vk_sample_settings_district_plan_{sample_idx:03d}_district_{district:02d}.json",
+                        "w",
+                    ) as out_file:
+                        json.dump(output_settings, out_file, indent=2)
